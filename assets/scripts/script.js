1c6329874a075c7198013025f8b5f357ca8351e0
mohamedsobhi70 <sobhi7070@gmail.com>
2023-06-20 05:28:20 +0300

    add

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -58,27 +58,27 @@ if ($(".slider-partners").length > 0) {
 
 
 
-
-if ($(".dropdown-list").length) {
-    $(".dropdown-list").hover(function (e) {
-        $(".colored-act").removeClass("active")
-        if ($(this).find(".dropdown-list-items").hasClass("active")) {
-            $(".dropdown-list-items").removeClass("active");
-            $(this).removeClass("active")
-        }
-        else {
-            $(".dropdown-list-items").removeClass("active");
-            $(this).find(".dropdown-list-items").addClass("active");
-            $(this).addClass("active")
-        }
-    })
-
-}
 // mobile dropdown 
 if ($(".menu-dropdown").length) {
-    $(".menu-dropdown").on("click", function (e) {
-        $(".menu-dropdown-list").slideToggle(500);
-    })
+    if ($(window).width() < 1023) {
+        $(".menu-dropdown").on("click", function (e) {
+            $(this).find(".menu-dropdown-list").slideToggle(500);
+        })
+    }
+    else{
+        $(".menu-dropdown").hover(function (e) {
+            $(".colored-act").removeClass("active")
+            if ($(this).find(".menu-dropdown-list").hasClass("active")) {
+                $(".menu-dropdown-list").removeClass("active");
+                $(this).removeClass("active")
+            }
+            else {
+                $(".menu-dropdown-list").removeClass("active");
+                $(this).find(".menu-dropdown-list").addClass("active");
+                $(this).addClass("active")
+            }
+        })
+    }
 }
 // =================================================
 
